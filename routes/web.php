df071ef9d594639bf2d56701496516e7bf8295a9
diusazzad <sazzad35-283@diu.edu.bd>
2023-11-08 15:47:27 +0600

    Initial commit

---
+++ routes/web.php
@@ -0,0 +1,46 @@
+<?php
+
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth','role:user', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+
+// Route::prefix('admin')->group(function(){
+//     Route::controller(AdminController::class)->group(function(){
+//         Route::get('/','index');
+//     });
+// });
+
+Route::prefix('admin')->middleware(['auth','role:administrator'])->group(function(){
+    Route::controller(AdminController::class)->group(function(){
+        Route::get('/','index');
+    });
+});
+
+
+require __DIR__.'/auth.php';
